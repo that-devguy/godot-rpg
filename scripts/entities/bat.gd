282fbf9950c97b22f919714695da0e1889649aa7
that devguy <zkmutch@gmail.com>
2024-10-30 23:36:37 -0400

    Trying a new way for setting up entities that will utilize state machines. Removed the base_entity scene and removed all scenes and scripts that were children of the base_entity.

--- scripts/entities/bat.gd
+++
@@ -1,5 +0,0 @@
-extends "res://scripts/entities/entity_base.gd"
-
-func _ready():
-	# Sets the initial idle animation when the scene loads
-	$AnimatedSprite2D.play("fly_down")
--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -1,93 +1,67 @@
-extends "res://scripts/entities/entity_base.gd"
+class_name Player extends CharacterBody2D
 
 # Variables
-var is_attacking = false
-var current_dir = ""
-var is_dead = false
+var cardinal_direction : Vector2 = Vector2.DOWN
+var direction: Vector2 = Vector2.ZERO
+var move_speed: float = 60
+var state : String = "idle"
+
+@onready var anim : AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready():
-	$AnimatedSprite2D.play("idle_down")
+	pass
 
-func _physics_process(_delta):
-	if not is_dead:  # Prevent player movement if dead
-		player_movement(_delta)
-
-# Handles player movement and direction
-func player_movement(_delta):
-	if Input.is_action_pressed("ui_up"):
-		current_dir = "up"
-		play_anim(1)
-		velocity.y = -speed
-		velocity.x = 0
-	elif Input.is_action_pressed("ui_down"):
-		current_dir = "down"
-		play_anim(1)
-		velocity.y = speed
-		velocity.x = 0
-	elif Input.is_action_pressed("ui_left"):
-		current_dir = "left"
-		play_anim(1)
-		velocity.x = -speed
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_right"):
-		current_dir = "right"
-		play_anim(1)
-		velocity.x = speed
-		velocity.y = 0
-	else:
-		play_anim(0)
-		velocity.y = 0
-		velocity.x = 0
+func _process(_delta):
+	# Handles player input and updates direction for movement
+	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
+	velocity = direction * move_speed
+	
+	# Updates animation if either state or direction has changed
+	if SetState() == true || SetDirection() == true:
+		UpdateAnim()
+
+func _physics_process(delta):
 	move_and_slide()
 
-# Handles player animations based on movement and direction
-func play_anim(movement):
-	var dir = current_dir
-	var anim = $AnimatedSprite2D
-
-	if dir == "up":
-		if movement == 1:
-			anim.play("walk_up")
-		elif movement == 0:
-			anim.play("idle_up")
-	if dir == "down":
-		if movement == 1:
-			anim.play("walk_down")
-		elif movement == 0:
-			anim.play("idle_down")
-	if dir == "left":
-		if movement == 1:
-			anim.play("walk_left")
-		elif movement == 0:
-			anim.play("idle_left")
-	if dir == "right":
-		if movement == 1:
-			anim.play("walk_right")
-		elif movement == 0:
-			anim.play("idle_right")
-
-func _on_hp_changed():
-	var dir = current_dir
-	var anim = $AnimatedSprite2D
+# Checks if the direction has changed and updates the cardinal direction if needed
+func SetDirection() -> bool:
+	var new_dir : Vector2 = cardinal_direction
+	if direction == Vector2.ZERO:
+		return false
 	
-	if dir == "up":
-		anim.play("damage_up")
-	if dir == "down":
-		anim.play("damage_down")
-	if dir == "left":
-		anim.play("damage_left")
-	if dir == "right":
-		anim.play("damage_right")
+	# Determines new direction based on input vector (Joystick friendly)
+	if direction.y == 0:
+		new_dir = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
+	elif direction.x == 0:
+		new_dir = Vector2.UP if direction.y < 0 else Vector2.DOWN
+	
+	if new_dir == cardinal_direction:
+		return false
+	
+	cardinal_direction = new_dir
+	return true
 
-func _on_died():
-	if not is_dead:  # Prevent multiple triggers
-		is_dead = true
-		$AnimatedSprite2D.play("death")
-		velocity = Vector2.ZERO
-		print("you died")
+# Determines if the state should change based on player movement
+func SetState() -> bool:
+	var new_state : String = "idle" if direction == Vector2.ZERO else "walk"
+	if new_state == state:
+		return false
+	state = new_state
+	return true
 
-func _on_animation_finished(anim_name):
-	if anim_name == "death":
-		# Handle what happens after death animation
-		queue_free()
+# Updates the animation based on the current state and facing direction
+func UpdateAnim() -> void:
+	anim.play(state + "_" + AnimDirection())
+
+# Determines the animation direction string based on the cardinal direction
+func AnimDirection() -> String:
+	if cardinal_direction == Vector2.DOWN:
+		return "down"
+	elif cardinal_direction == Vector2.UP:
+		return "up"
+	elif cardinal_direction == Vector2.LEFT:
+		return "left"
+	else:
+		return "right"
